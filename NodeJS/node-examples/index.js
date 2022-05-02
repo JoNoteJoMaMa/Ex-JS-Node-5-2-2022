970af058228e5a25ad43cd3edee1e0ad84e42150
bbbung <kanyapat.mekv@kmutt.ac.th>
2022-05-02 18:07:09 +0700

    Submit work

--- NodeJS/node-examples/index.js
+++ NodeJS/node-examples/index.js
@@ -1,27 +1,6 @@
-var rect = {
-    perimeter: (x, y) => (2*(x+y)),
-    area: (x, y) => (x*y)
-   };
-   function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
-    if (l <= 0 || b <= 0) {
-    console.log("Rectangle dimensions should be greater than zero: l = "
-    + l + ", and b = " + b);
-    }
-    else {
-    console.log("The area of the rectangle is " + rect.area(l,b));
-    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b)
-   );
-    }
-   }
-   solveRect(2,4);
-   solveRect(3,5);
-   solveRect(0,5);
-   solveRect(-3,5);
+var rect = require('./rectangle');
 
-   var rect = require('./rectangle');
-
-   function solveRect(l,b) {
+function solveRect(l,b) {
     console.log("Solving for rectangle with l = "
     + l + " and b = " + b);
     rect(l,b, (err,rectangle) => {
@@ -37,3 +16,10 @@ var rect = {
     });
     console.log("This statement after the call to rect()");
    };
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
+
+
+
